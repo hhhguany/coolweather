103dfe0f7ee05aa438958d36888989a8f023c434
udidi <hhhguany@me.com>
2017-03-21 18:20:45 +0800

    Create database & tables & config

---
+++ app/src/androidTest/java/org/udidi/coolweather/db/City.java
@@ -0,0 +1,46 @@
+package org.udidi.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by He.RO on 2017/3/21.
+ */
+
+public class City extends DataSupport {
+    private int id;
+    private String cityName;
+    private int cityCode;
+    private int provinceId;
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
